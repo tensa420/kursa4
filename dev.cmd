5c9942224c32e8a2a0b724649eefd858a934e55a
Oleg <gnomnano97@yandex.ru>
2026-05-21 22:08:37 +0300

    Revert "project"
    This reverts commit c6cfb8e8c47e622adaa3cc6e2615285e99e0383f.

--- dev.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Запуск без политики PowerShell (обходит PSSecurityException)
-cd /d "%~dp0"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0run-local.ps1" %*
-if errorlevel 1 (
-    echo.
-    echo Error. Press any key...
-    pause >nul
-)
--- web/src/lib/productImage.ts
+++
@@ -1,4 +0,0 @@
-import { PRODUCT_IMAGES } from "./productImages";
-
-/** Запасное фото, если файл товара не найден (не молоко для всех подряд). */
-export const PRODUCT_IMG_FALLBACK = PRODUCT_IMAGES.p1;
